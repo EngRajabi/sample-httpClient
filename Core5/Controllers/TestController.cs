8e562a281e07f04035245e1427bd8a2466c2dd55
Mohsen Rajabi <m.kabir8895@gmail.com>
2020-11-17 20:10:06 +0330

    init

---
+++ Core5/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestController(ILogger<TestController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("test");
+
+                var httpResponseMessage = await client.GetAsync("http://localhost:8080", cancellationToken);
+
+                var con = await httpResponseMessage.Content.ReadAsStringAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+
+
+            return Ok();
+        }
+    }
+}
